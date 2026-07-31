0eabe9375ed12ae84ac97484b54b5a945e143b82
Anita <anitakarimi953@gmail.com>
2026-07-31 22:47:06 +0330

    add Message class

---
+++ src/main/java/common/Message.java
@@ -0,0 +1,20 @@
+package Ap404.common;
+
+public class Message {
+
+    private String type;
+    private String content;
+
+    public Message(String type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
